fea1056753432e58bf40488c6ad982b4c8ba7e84
Nik Bailo <ngbailo@gmail.com>
2020-10-19 08:02:19 +0300

    Update reward calculation

--- contracts/IndexStaking.sol
+++ contracts/IndexStaking.sol
@@ -2,29 +2,33 @@
 pragma solidity ^0.6.6;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "../interfaces/IIndexHybridToken.sol";
-import "../interfaces/IHybridToken.sol";
 import "../libraries/SafeTransfer.sol";
 
 contract IndexStaking {
     using SafeMath for uint256;
 
+    uint public constant duration = 3 * 365 * 6500; // blocks
+    uint public constant totalSupply = 100000000 * 10**18;
+    uint public constant rewardSupply = 48 * totalSupply / 100;
+
     address public stakingToken;
     address public rewardToken;
 
-    // init T, S, stake, S0
     uint public staked; // S
     uint public activeStakeDeposits; // T
     mapping(address => uint) public stake; // stake
     mapping(address => uint) public stakedSnapshot; // S0
 
+    uint public accumulatedReward;
+    uint public startedBlock;
+
     constructor(address _stakingTokenAddress, address _rewardTokenAddress) public {
         stakingToken = _stakingTokenAddress;
         rewardToken = _rewardTokenAddress;
+        startedBlock = block.number;
     }
 
     function deposit(uint _amount) external {
-        _update();
         SafeTransfer.transferFromERC20(address(stakingToken), msg.sender, address(this), _amount);
         stake[msg.sender] = _amount;
         stakedSnapshot[msg.sender] = staked;
@@ -32,25 +36,26 @@ contract IndexStaking {
     }
 
     function withdraw() external {
-        _update();
         uint deposited = stake[msg.sender];
-        uint reward = deposited.mul(staked.sub(stakedSnapshot[msg.sender]));
-        activeStakeDeposits = activeStakeDeposits.sub(deposited);
-        stake[msg.sender] = 0;
-        SafeTransfer.sendERC20(address(stakingToken), msg.sender, deposited);
-        SafeTransfer.sendERC20(address(rewardToken), msg.sender, reward);
-    }
-
-
-    function _update() private {
         uint reward = _calculateReward();
         if (activeStakeDeposits != 0) {
             staked = staked.add(reward.div(activeStakeDeposits));
         }
+        uint userReward = deposited.mul(staked.sub(stakedSnapshot[msg.sender]));
+        activeStakeDeposits = activeStakeDeposits.sub(deposited);
+        stake[msg.sender] = 0;
+        SafeTransfer.sendERC20(address(stakingToken), msg.sender, deposited);
+        SafeTransfer.sendERC20(address(rewardToken), msg.sender, userReward);
     }
 
-    function _calculateReward() private returns (uint) {
-        // k = 1
-        // r = k * reward share * total supply (N current block - N block last reward)
+    function _calculateReward() private returns (uint reward) {
+        if (startedBlock.add(duration) > block.number) {
+            reward = rewardSupply
+                .mul(block.number)
+                .mul(block.number.sub(startedBlock))
+                .div((startedBlock.add(duration)).mul(duration));
+        }
+        reward -= accumulatedReward;
+        accumulatedReward += reward;
     }
 }
