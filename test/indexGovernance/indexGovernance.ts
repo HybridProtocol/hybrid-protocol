66178c0d86f6e34749a142d4a833548d86126b02
kampod <v.kinzeryavyy@gmail.com>
2020-10-23 21:40:56 +0300

    Update tests for function 'IndexGovernance' of contract IndexGovernance.sol

--- test/indexGovernance/indexGovernance.ts
+++ test/indexGovernance/indexGovernance.ts
@@ -4,7 +4,7 @@ import { Contract } from 'ethers';
 import { IndexHybridToken } from '../../typechain/IndexHybridToken';
 import { IndexGovernance } from '../../typechain/IndexGovernance';
 import { indexGovernanceFixture, indexGovernanceMinDuration } from './indexGovernanceFixtures';
-import { convertStringToArrayish, expandTo18Decimals } from '../shared/utilities';
+import { convertStringToArrayish, expandTo18Decimals, mineBlocks } from '../shared/utilities';
 
 chai.use(solidity);
 
@@ -12,6 +12,7 @@ const ERRORS = {
   INVALID_LENGTH: 'IndexGovernance: INVALID_LENGTH',
   DURATION_INVALID: 'IndexGovernance: DURATION_INVALID',
   TOTAL_WEIGHTS: 'IndexGovernance: TOTAL_WEIGHTS',
+  VOTING_IN_PROGRESS: 'IndexGovernance: VOTING_IN_PROGRESS',
 };
 
 describe('IndexGovernance', () => {
@@ -88,6 +89,7 @@ describe('IndexGovernance', () => {
       duration: indexGovernanceMinDuration * 2,
     },
   };
+  const nullableAddress = '0x0000000000000000000000000000000000000000';
 
   beforeEach(async () => {
     // load fixture
@@ -99,7 +101,6 @@ describe('IndexGovernance', () => {
 
     // init test action
     await indexHybridToken.addAddressesToMainteiners([aliceWallet.address]); // add aliceWallet.address to mainteiners
-
     await indexHybridToken
       .connect(aliceWallet)
       .mintAmount(
@@ -113,11 +114,9 @@ describe('IndexGovernance', () => {
         ],
         expandTo18Decimals(1000),
       ); // send xHBT to address list
-
     await indexHybridToken
       .connect(aliceWallet)
       .updateComposition(voteProposalAssets.start.assets, voteProposalAssets.start.weights); // set start composition
-
     await indexGovernance.addAddressesToMainteiners([aliceWallet.address, bobWallet.address]); // add aliceWallet.address and bobWallet.address to mainteiners
   });
 
@@ -137,17 +136,25 @@ describe('IndexGovernance', () => {
       ).to.be.reverted;
     });
 
-    it.skip('fail - invalid assets name', async () => {
+    it('fail - invalid assets name', async () => {
       // get voteAssets
       const voteAssets = voteProposalAssets.invalidAssetName;
 
       // check contract maintainers - maintainer
       await checkAddressMainteiner(aliceWallet.address, indexGovernance, true);
 
-      // run method createProposal() - reverted
-      await expect(
-        indexGovernance.connect(aliceWallet).createProposal(voteAssets.assets, voteAssets.weights, voteAssets.duration),
-      ).to.be.throw;
+      // run method createProposal() - throw runtime error
+      let error: any;
+      try {
+        await indexGovernance
+          .connect(aliceWallet)
+          .createProposal(voteAssets.assets, voteAssets.weights, voteAssets.duration);
+      } catch (e) {
+        error = e;
+      }
+      expect(error).not.to.be.undefined;
+      expect(error.reason).to.be.eq('invalid bytes8 value');
+      expect(error.code).to.be.eq('INVALID_ARGUMENT');
     });
 
     it('fail - assets and weights has different length (IndexGovernance: INVALID_LENGTH)', async () => {
@@ -202,23 +209,88 @@ describe('IndexGovernance', () => {
       ).to.be.revertedWith(ERRORS.TOTAL_WEIGHTS);
     });
 
-    it('success - one createProposal', async () => {
+    it('fail - cannot create second proposal before completing the first (IndexGovernance: VOTING_IN_PROGRESS)', async () => {
       // get voteAssets
       const voteAssets = voteProposalAssets.base;
 
-      // check contract maintainers - maintainer
+      // check contract maintainers - maintainers
       await checkAddressMainteiner(aliceWallet.address, indexGovernance, true);
+      await checkAddressMainteiner(bobWallet.address, indexGovernance, true);
 
+      // get and check beforeProposal
       const beforeProposal = await indexGovernance.proposal();
-      console.log(beforeProposal);
+      expect(beforeProposal.id.toNumber()).to.be.eq(0);
+      expect(beforeProposal.initiator).to.be.eq(nullableAddress);
 
       // run method createProposal() - successfully
       await expect(
         indexGovernance.connect(aliceWallet).createProposal(voteAssets.assets, voteAssets.weights, voteAssets.duration),
       ).not.to.be.reverted;
 
+      // run method createProposal() - reverted
+      await expect(
+        indexGovernance.connect(bobWallet).createProposal(voteAssets.assets, voteAssets.weights, voteAssets.duration),
+      ).to.be.revertedWith(ERRORS.VOTING_IN_PROGRESS);
+
+      // get and check afterProposal
       const afterProposal = await indexGovernance.proposal();
-      console.log(afterProposal);
+      expect(afterProposal.id.toNumber()).to.be.eq(beforeProposal.id.toNumber() + 1);
+      expect(afterProposal.initiator).to.be.eq(aliceWallet.address);
+    });
+
+    it('success - create one proposal', async () => {
+      // get voteAssets
+      const voteAssets = voteProposalAssets.base;
+
+      // check contract maintainers - maintainer
+      await checkAddressMainteiner(aliceWallet.address, indexGovernance, true);
+
+      // get and check beforeProposal
+      const beforeProposal = await indexGovernance.proposal();
+      expect(beforeProposal.id.toNumber()).to.be.eq(0);
+      expect(beforeProposal.initiator).to.be.eq(nullableAddress);
+
+      // run method createProposal() - successfully
+      await expect(
+        indexGovernance.connect(aliceWallet).createProposal(voteAssets.assets, voteAssets.weights, voteAssets.duration),
+      ).not.to.be.reverted;
+
+      // get and check afterProposal
+      const afterProposal = await indexGovernance.proposal();
+      expect(afterProposal.id.toNumber()).to.be.eq(beforeProposal.id.toNumber() + 1);
+      expect(afterProposal.initiator).to.be.eq(aliceWallet.address);
+    });
+
+    it('success - create second proposal after completing the first', async () => {
+      // get voteAssets
+      const voteAssets = voteProposalAssets.base;
+
+      // check contract maintainers - maintainers
+      await checkAddressMainteiner(aliceWallet.address, indexGovernance, true);
+      await checkAddressMainteiner(bobWallet.address, indexGovernance, true);
+
+      // get and check beforeProposal
+      const beforeProposal = await indexGovernance.proposal();
+      expect(beforeProposal.id.toNumber()).to.be.eq(0);
+      expect(beforeProposal.initiator).to.be.eq(nullableAddress);
+
+      // run method createProposal() - successfully
+      await expect(
+        indexGovernance.connect(aliceWallet).createProposal(voteAssets.assets, voteAssets.weights, voteAssets.duration),
+      ).not.to.be.reverted;
+
+      // mine blocks
+      await mineBlocks(provider, voteAssets.duration);
+
+      // run method createProposal() - successfully
+      await expect(
+        indexGovernance.connect(bobWallet).createProposal(voteAssets.assets, voteAssets.weights, voteAssets.duration),
+      ).not.to.be.reverted;
+
+      // get and check afterProposal
+      const afterProposal = await indexGovernance.proposal();
+      expect(afterProposal.id.toNumber()).to.be.eq(beforeProposal.id.toNumber() + 2);
+      expect(afterProposal.initiator).to.be.eq(bobWallet.address);
     });
   });
 });
