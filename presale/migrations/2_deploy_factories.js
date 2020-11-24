686e60ee301850239c93735266646e91ab9dc326
Nik Bailo <ngbailo@gmail.com>
2020-11-24 05:24:10 +0200

    HP-41 - Add presale contracts

---
+++ presale/migrations/2_deploy_factories.js
@@ -0,0 +1,27 @@
+const RightsManager = artifacts.require('RightsManager');
+const SmartPoolManager = artifacts.require('SmartPoolManager');
+const CRPFactory = artifacts.require('CRPFactory');
+const ESPFactory = artifacts.require('ESPFactory');
+const BFactory = artifacts.require('BFactory');
+const BalancerSafeMath = artifacts.require('BalancerSafeMath');
+const BalancerSafeMathMock = artifacts.require('BalancerSafeMathMock');
+
+module.exports = async function (deployer, network, accounts) {
+    if (network === 'development' || network === 'coverage') {
+        await deployer.deploy(RightsManager);
+        await deployer.deploy(SmartPoolManager);
+        await deployer.deploy(BFactory);
+        await deployer.deploy(BalancerSafeMath);
+        await deployer.deploy(BalancerSafeMathMock);
+    }
+
+    await deployer.deploy(RightsManager);
+    await deployer.deploy(SmartPoolManager);
+    await deployer.deploy(BalancerSafeMath);
+
+    deployer.link(BalancerSafeMath, CRPFactory);
+    deployer.link(RightsManager, CRPFactory);
+    deployer.link(SmartPoolManager, CRPFactory);
+    
+    await deployer.deploy(CRPFactory);
+};
