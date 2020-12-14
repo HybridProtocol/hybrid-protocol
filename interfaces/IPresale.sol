18c555326caa36c33b57bfc1ac91bf8acd490097
Nik Bailo <ngbailo@gmail.com>
2020-12-14 09:14:21 +0200

    Add tests for Vesting Swap

--- interfaces/IPresale.sol
+++ interfaces/IPresale.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.6.6;
 
 interface IPresale {
     function start() external;
-    function burn() external;
     function sendUSDC(address _to, uint _amount) external;
     function buy(uint _amountUSDC) external;
     function purchasedAmount(address _account) external view returns (uint);
